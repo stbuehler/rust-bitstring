7dcbb7c1dfc5bff58a2b1060db6f26bac7e4f05b
Stefan Bühler <source@stbuehler.de>
2024-09-08 12:33:24 +0200

    bigendian utils: expose BigEndianBitString functions outside of trait to allow const implementations

--- src/utils/bigendian.rs
+++ src/utils/bigendian.rs
@@ -1,17 +1,10 @@
-use std::{
-	cmp::min,
-	mem::size_of,
-};
-
 /// Generic helper methods to treat unsigned integer `u*`-slices as big endian bit strings.
 pub trait BigEndianBitString: Sized {
 	/// bits in a single element
-	fn elembits() -> usize {
-		8 * size_of::<Self>()
-	}
+	const ELEMENT_BITS: usize;
 
 	/// integer with single bit set. bit 0 is the highest bit (big
-	/// endian).  Wraps at `Self::elembits()`.
+	/// endian).  Wraps at `Self::ELEMENT_BITS()`.
 	fn mask(ndx: usize) -> Self;
 
 	/// increment from right; don't touch first `prefix` bits; returns
@@ -19,48 +12,42 @@ pub trait BigEndianBitString: Sized {
 	///
 	/// # Panics
 	///
-	/// Panics if `prefix > Self::elembits() * slice.len()`.
+	/// Panics if `prefix > Self::ELEMENT_BITS() * slice.len()`.
 	fn inc(slice: &mut [Self], prefix: usize) -> bool;
 
 	/// Get the `ndx`th bit.
 	///
 	/// # Panics
 	///
-	/// Panics if `ndx >= Self::elembits() * slice.len()`.
+	/// Panics if `ndx >= Self::ELEMENT_BITS() * slice.len()`.
 	fn get(slice: &[Self], ndx: usize) -> bool;
 
 	/// Set the `ndx`th bit to `bit`.
 	///
 	/// # Panics
 	///
-	/// Panics if `ndx >= Self::elembits() * slice.len()`.
-	fn set(slice: &mut [Self], ndx: usize, bit: bool) {
-		if bit {
-			Self::on(slice, ndx)
-		} else {
-			Self::off(slice, ndx)
-		}
-	}
+	/// Panics if `ndx >= Self::ELEMENT_BITS() * slice.len()`.
+	fn set(slice: &mut [Self], ndx: usize, bit: bool);
 
 	/// Set the `ndx`th bit to `true`.
 	///
 	/// # Panics
 	///
-	/// Panics if `ndx >= Self::elembits() * slice.len()`.
+	/// Panics if `ndx >= Self::ELEMENT_BITS() * slice.len()`.
 	fn on(slice: &mut [Self], ndx: usize);
 
 	/// Set the `ndx`th bit to `false`.
 	///
 	/// # Panics
 	///
-	/// Panics if `ndx >= Self::elembits() * slice.len()`.
+	/// Panics if `ndx >= Self::ELEMENT_BITS() * slice.len()`.
 	fn off(slice: &mut [Self], ndx: usize);
 
 	/// Flips the `ndx`th bit.
 	///
 	/// # Panics
 	///
-	/// Panics if `ndx >= Self::elembits() * slice.len()`.
+	/// Panics if `ndx >= Self::ELEMENT_BITS() * slice.len()`.
 	fn flip(slice: &mut [Self], ndx: usize);
 
 	/// Length of the longest shared prefix of two bit strings.
@@ -68,22 +55,22 @@ pub trait BigEndianBitString: Sized {
 
 	/// Set all bits from [ndx..] to `false`.
 	///
-	/// Doesn't do anything if `ndx >= Self::elembits() * slice.len()`.
+	/// Doesn't do anything if `ndx >= Self::ELEMENT_BITS() * slice.len()`.
 	fn set_false_from(slice: &mut [Self], ndx: usize);
 
 	/// Whether all bits from [ndx..] are `false`.
 	///
-	/// Returns `true` if `ndx >= Self::elembits() * slice.len()`.
+	/// Returns `true` if `ndx >= Self::ELEMENT_BITS() * slice.len()`.
 	fn is_false_from(slice: &[Self], ndx: usize) -> bool;
 
 	/// Set all bits from [ndx..] to `true`.
 	///
-	/// Doesn't do anything if `ndx >= Self::elembits() * slice.len()`.
+	/// Doesn't do anything if `ndx >= Self::ELEMENT_BITS() * slice.len()`.
 	fn set_true_from(slice: &mut [Self], ndx: usize);
 
 	/// Whether all bits from [ndx..] are `true`.
 	///
-	/// Returns `true` if `ndx >= Self::elembits() * slice.len()`.
+	/// Returns `true` if `ndx >= Self::ELEMENT_BITS() * slice.len()`.
 	fn is_true_from(slice: &[Self], ndx: usize) -> bool;
 
 	/// check whether another bit string `other` shares the first
@@ -92,19 +79,35 @@ pub trait BigEndianBitString: Sized {
 }
 
 macro_rules! impl_big_endian_for {
-	($t:ty) => {
-		impl BigEndianBitString for $t {
-			fn mask(ndx: usize) -> Self {
-				let bits = Self::elembits();
-				let bit_ndx = bits - 1 - (ndx % bits);
+	($mod:ident => $t:ty) => {
+		/// `BigEndianBitString` functions for specific integer type
+		pub mod $mod {
+			use std::{
+				cmp::min,
+				mem::size_of,
+			};
+
+			/// bits in a single element
+			pub const ELEMENT_BITS: usize = 8 * size_of::<$t>();
+
+			/// integer with single bit set. bit 0 is the highest bit (big
+			/// endian).  Wraps at `ELEMENT_BITS`.
+			pub const fn mask(ndx: usize) -> $t {
+				let bit_ndx = ELEMENT_BITS - 1 - (ndx % ELEMENT_BITS);
 				1 << bit_ndx
 			}
 
-			fn inc(slice: &mut [Self], prefix: usize) -> bool {
-				assert!(prefix <= Self::elembits() * slice.len());
+			/// increment from right; don't touch first `prefix` bits; returns
+			/// true on overflow
+			///
+			/// # Panics
+			///
+			/// Panics if `prefix > ELEMENT_BITS * slice.len()`.
+			pub fn inc(slice: &mut [$t], prefix: usize) -> bool {
+				assert!(prefix <= ELEMENT_BITS * slice.len());
 				// first element that might be touched by overflow
-				let slice_ndx = prefix / Self::elembits();
-				if 0 == prefix % Self::elembits() {
+				let slice_ndx = prefix / ELEMENT_BITS;
+				if 0 == prefix % ELEMENT_BITS {
 					// just don't overflow into elems before slice_ndx
 
 					let mut overflow = true;
@@ -120,7 +123,7 @@ macro_rules! impl_big_endian_for {
 				} else {
 					// on overflow set all bits to false from "prefix"th bit
 
-					let last_prefix_bit_mask = Self::mask(prefix - 1);
+					let last_prefix_bit_mask = mask(prefix - 1);
 					let overflow_mask = last_prefix_bit_mask - 1;
 					let overflow_elem = slice[slice_ndx] & !overflow_mask;
 
@@ -151,61 +154,98 @@ macro_rules! impl_big_endian_for {
 				}
 			}
 
-			fn get(slice: &[Self], ndx: usize) -> bool {
-				let mask = Self::mask(ndx);
-				let slice_ndx = ndx / Self::elembits();
+			/// Get the `ndx`th bit.
+			///
+			/// # Panics
+			///
+			/// Panics if `ndx >= ELEMENT_BITS * slice.len()`.
+			pub const fn get(slice: &[$t], ndx: usize) -> bool {
+				let mask = mask(ndx);
+				let slice_ndx = ndx / ELEMENT_BITS;
 				0 != (slice[slice_ndx] & mask)
 			}
 
-			fn on(slice: &mut [Self], ndx: usize) {
-				let mask = Self::mask(ndx);
-				let slice_ndx = ndx / Self::elembits();
+			/// Set the `ndx`th bit to `bit`.
+			///
+			/// # Panics
+			///
+			/// Panics if `ndx >= ELEMENT_BITS * slice.len()`.
+			pub fn set(slice: &mut [$t], ndx: usize, bit: bool) {
+				if bit {
+					on(slice, ndx)
+				} else {
+					off(slice, ndx)
+				}
+			}
+
+			/// Set the `ndx`th bit to `true`.
+			///
+			/// # Panics
+			///
+			/// Panics if `ndx >= ELEMENT_BITS * slice.len()`.
+			pub fn on(slice: &mut [$t], ndx: usize) {
+				let mask = mask(ndx);
+				let slice_ndx = ndx / ELEMENT_BITS;
 				slice[slice_ndx] |= mask;
 			}
 
-			fn off(slice: &mut [Self], ndx: usize) {
-				let mask = Self::mask(ndx);
-				let slice_ndx = ndx / Self::elembits();
+			/// Set the `ndx`th bit to `false`.
+			///
+			/// # Panics
+			///
+			/// Panics if `ndx >= ELEMENT_BITS * slice.len()`.
+			pub fn off(slice: &mut [$t], ndx: usize) {
+				let mask = mask(ndx);
+				let slice_ndx = ndx / ELEMENT_BITS;
 				slice[slice_ndx] &= !mask;
 			}
 
-			fn flip(slice: &mut [Self], ndx: usize) {
-				let mask = Self::mask(ndx);
-				let slice_ndx = ndx / Self::elembits();
+			/// Flips the `ndx`th bit.
+			///
+			/// # Panics
+			///
+			/// Panics if `ndx >= ELEMENT_BITS * slice.len()`.
+			pub fn flip(slice: &mut [$t], ndx: usize) {
+				let mask = mask(ndx);
+				let slice_ndx = ndx / ELEMENT_BITS;
 				slice[slice_ndx] ^= mask;
 			}
 
-			fn shared_prefix_len(slice: &[Self], other: &[Self], max_len: usize) -> usize {
+			/// Length of the longest shared prefix of two bit strings.
+			pub fn shared_prefix_len(slice: &[$t], other: &[$t], max_len: usize) -> usize {
 				if 0 == max_len {
 					return 0;
 				}
 				// slice index of last bit to compare
-				let slice_ndx = (max_len - 1) / Self::elembits();
+				let slice_ndx = (max_len - 1) / ELEMENT_BITS;
 				for i in 0..slice_ndx {
 					let diff = slice[i] ^ other[i];
 					if 0 != diff {
-						return i * Self::elembits() + diff.leading_zeros() as usize;
+						return i * ELEMENT_BITS + diff.leading_zeros() as usize;
 					}
 				}
 				let diff = slice[slice_ndx] ^ other[slice_ndx];
 				if 0 != diff {
 					min(
 						max_len,
-						slice_ndx * Self::elembits() + diff.leading_zeros() as usize,
+						slice_ndx * ELEMENT_BITS + diff.leading_zeros() as usize,
 					)
 				} else {
 					max_len
 				}
 			}
 
-			fn set_false_from(slice: &mut [Self], ndx: usize) {
-				let slice_ndx = ndx / Self::elembits();
-				if 0 == ndx % Self::elembits() {
+			/// Set all bits from [ndx..] to `false`.
+			///
+			/// Doesn't do anything if `ndx >= ELEMENT_BITS * slice.len()`.
+			pub fn set_false_from(slice: &mut [$t], ndx: usize) {
+				let slice_ndx = ndx / ELEMENT_BITS;
+				if 0 == ndx % ELEMENT_BITS {
 					for i in slice_ndx..slice.len() {
 						slice[i] = 0;
 					}
 				} else if slice_ndx < slice.len() {
-					let mask = Self::mask(ndx - 1) - 1;
+					let mask = mask(ndx - 1) - 1;
 					slice[slice_ndx] &= !mask;
 					for i in slice_ndx + 1..slice.len() {
 						slice[i] = 0;
@@ -213,16 +253,19 @@ macro_rules! impl_big_endian_for {
 				}
 			}
 
-			fn is_false_from(slice: &[Self], ndx: usize) -> bool {
-				let slice_ndx = ndx / Self::elembits();
-				if 0 == ndx % Self::elembits() {
+			/// Whether all bits from [ndx..] are `false`.
+			///
+			/// Returns `true` if `ndx >= ELEMENT_BITS * slice.len()`.
+			pub fn is_false_from(slice: &[$t], ndx: usize) -> bool {
+				let slice_ndx = ndx / ELEMENT_BITS;
+				if 0 == ndx % ELEMENT_BITS {
 					for i in slice_ndx..slice.len() {
 						if 0 != slice[i] {
 							return false;
 						}
 					}
 				} else if slice_ndx < slice.len() {
-					let mask = Self::mask(ndx - 1) - 1;
+					let mask = mask(ndx - 1) - 1;
 					if 0 != slice[slice_ndx] & mask {
 						return false;
 					}
@@ -235,14 +278,17 @@ macro_rules! impl_big_endian_for {
 				true
 			}
 
-			fn set_true_from(slice: &mut [Self], ndx: usize) {
-				let slice_ndx = ndx / Self::elembits();
-				if 0 == ndx % Self::elembits() {
+			/// Set all bits from [ndx..] to `true`.
+			///
+			/// Doesn't do anything if `ndx >= ELEMENT_BITS * slice.len()`.
+			pub fn set_true_from(slice: &mut [$t], ndx: usize) {
+				let slice_ndx = ndx / ELEMENT_BITS;
+				if 0 == ndx % ELEMENT_BITS {
 					for i in slice_ndx..slice.len() {
 						slice[i] = !0;
 					}
 				} else if slice_ndx < slice.len() {
-					let mask = Self::mask(ndx - 1) - 1;
+					let mask = mask(ndx - 1) - 1;
 					slice[slice_ndx] |= mask;
 					for i in slice_ndx + 1..slice.len() {
 						slice[i] = !0;
@@ -250,16 +296,19 @@ macro_rules! impl_big_endian_for {
 				}
 			}
 
-			fn is_true_from(slice: &[Self], ndx: usize) -> bool {
-				let slice_ndx = ndx / Self::elembits();
-				if 0 == ndx % Self::elembits() {
+			/// Whether all bits from [ndx..] are `true`.
+			///
+			/// Returns `true` if `ndx >= ELEMENT_BITS * slice.len()`.
+			pub fn is_true_from(slice: &[$t], ndx: usize) -> bool {
+				let slice_ndx = ndx / ELEMENT_BITS;
+				if 0 == ndx % ELEMENT_BITS {
 					for i in slice_ndx..slice.len() {
 						if slice[i] != !0 {
 							return false;
 						}
 					}
 				} else if slice_ndx < slice.len() {
-					let mask = Self::mask(ndx - 1) - 1;
+					let mask = mask(ndx - 1) - 1;
 					if slice[slice_ndx] | !mask != !0 {
 						return false;
 					}
@@ -272,28 +321,86 @@ macro_rules! impl_big_endian_for {
 				true
 			}
 
-			fn contains(slice: &[Self], prefix: usize, other: &[Self]) -> bool {
-				let slice_ndx = prefix / Self::elembits();
+			/// check whether another bit string `other` shares the first
+			/// `prefix` bits with `self`
+			pub fn contains(slice: &[$t], prefix: usize, other: &[$t]) -> bool {
+				let slice_ndx = prefix / ELEMENT_BITS;
 				for i in 0..slice_ndx {
 					if slice[i] != other[i] {
 						return false;
 					}
 				}
-				if 0 == prefix % Self::elembits() {
+				if 0 == prefix % ELEMENT_BITS {
 					return true;
 				}
-				let mask = !(Self::mask(prefix - 1) - 1);
+				let mask = !(mask(prefix - 1) - 1);
 				0 == mask & (slice[slice_ndx] ^ other[slice_ndx])
+			}
+		}
+
+		impl BigEndianBitString for $t {
+			const ELEMENT_BITS: usize = $mod::ELEMENT_BITS;
+
+			fn mask(ndx: usize) -> Self {
+				$mod::mask(ndx)
+			}
+
+			fn inc(slice: &mut [Self], prefix: usize) -> bool {
+				$mod::inc(slice, prefix)
+			}
+
+			fn get(slice: &[Self], ndx: usize) -> bool {
+				$mod::get(slice, ndx)
+			}
+
+			fn set(slice: &mut [Self], ndx: usize, bit: bool) {
+				$mod::set(slice, ndx, bit)
+			}
+
+			fn on(slice: &mut [Self], ndx: usize) {
+				$mod::on(slice, ndx)
+			}
+
+			fn off(slice: &mut [Self], ndx: usize) {
+				$mod::off(slice, ndx)
+			}
+
+			fn flip(slice: &mut [Self], ndx: usize) {
+				$mod::flip(slice, ndx)
+			}
+
+			fn shared_prefix_len(slice: &[Self], other: &[Self], max_len: usize) -> usize {
+				$mod::shared_prefix_len(slice, other, max_len)
+			}
+
+			fn set_false_from(slice: &mut [Self], ndx: usize) {
+				$mod::set_false_from(slice, ndx)
+			}
+
+			fn is_false_from(slice: &[Self], ndx: usize) -> bool {
+				$mod::is_false_from(slice, ndx)
+			}
+
+			fn set_true_from(slice: &mut [Self], ndx: usize) {
+				$mod::set_true_from(slice, ndx)
+			}
+
+			fn is_true_from(slice: &[Self], ndx: usize) -> bool {
+				$mod::is_true_from(slice, ndx)
+			}
+
+			fn contains(slice: &[Self], prefix: usize, other: &[Self]) -> bool {
+				$mod::contains(slice, prefix, other)
 			}
 		}
 	};
 }
 
-impl_big_endian_for! {u8}
-impl_big_endian_for! {u16}
-impl_big_endian_for! {u32}
-impl_big_endian_for! {u64}
-impl_big_endian_for! {u128}
+impl_big_endian_for! {u8 => u8}
+impl_big_endian_for! {u16 => u16}
+impl_big_endian_for! {u32 => u32}
+impl_big_endian_for! {u64 => u64}
+impl_big_endian_for! {u128 => u128}
 
 #[cfg(test)]
 mod tests {
