932b5491987aedd9494e9e5b4d73e7b4b2da7ebb
Stefan Bühler <source@stbuehler.de>
2024-09-09 18:20:29 +0200

    Use u32/u128 bit operations for some methods in the FixedBitString impls for Ipv4Addr and Ipv6Addr

--- src/address.rs
+++ src/address.rs
@@ -18,9 +18,19 @@ where
 	result
 }
 
+fn with_ipv4_mut_u32<F, T>(addr: &mut Ipv4Addr, f: F) -> T
+where
+	F: FnOnce(&mut u32) -> T,
+{
+	let mut o = addr.to_bits();
+	let result = f(&mut o);
+	*addr = Ipv4Addr::from_bits(o);
+	result
+}
+
 impl FixedBitString for Ipv4Addr {
 	fn inc(&mut self, prefix: usize) -> bool {
-		with_ipv4_mut_slice(self, |slice| BigEndianBitString::bits_inc(slice, prefix))
+		with_ipv4_mut_u32(self, |num| num.bits_inc(prefix))
 	}
 
 	fn len() -> usize {
@@ -28,47 +38,47 @@ impl FixedBitString for Ipv4Addr {
 	}
 
 	fn get(&self, ndx: usize) -> bool {
-		BigEndianBitString::bit_get(&self.octets()[..], ndx)
+		self.octets().as_slice().bit_get(ndx)
 	}
 
 	fn set(&mut self, ndx: usize, bit: bool) {
-		with_ipv4_mut_slice(self, |slice| BigEndianBitString::bit_set(slice, ndx, bit))
+		with_ipv4_mut_slice(self, |slice| slice.bit_set(ndx, bit))
 	}
 
 	fn flip(&mut self, ndx: usize) {
-		with_ipv4_mut_slice(self, |slice| BigEndianBitString::bit_flip(slice, ndx))
+		with_ipv4_mut_slice(self, |slice| slice.bit_flip(ndx))
 	}
 
 	fn shared_prefix_len(&self, other: &Self, max_len: usize) -> usize {
-		BigEndianBitString::shared_prefix_len(&self.octets()[..], &other.octets()[..], max_len)
+		self.to_bits().shared_prefix_len(&other.to_bits(), max_len)
 	}
 
 	fn set_false_from(&mut self, ndx: usize) {
-		with_ipv4_mut_slice(self, |slice| BigEndianBitString::set_false_from(slice, ndx))
+		with_ipv4_mut_u32(self, |num| num.set_false_from(ndx))
 	}
 
 	fn is_false_from(&self, ndx: usize) -> bool {
-		BigEndianBitString::is_false_from(&self.octets()[..], ndx)
+		self.to_bits().is_false_from(ndx)
 	}
 
 	fn set_true_from(&mut self, ndx: usize) {
-		with_ipv4_mut_slice(self, |slice| BigEndianBitString::set_true_from(slice, ndx))
+		with_ipv4_mut_u32(self, |num| num.set_true_from(ndx))
 	}
 
 	fn is_true_from(&self, ndx: usize) -> bool {
-		BigEndianBitString::is_true_from(&self.octets()[..], ndx)
+		self.to_bits().is_true_from(ndx)
 	}
 
 	fn new_all_false() -> Self {
-		Ipv4Addr::new(0, 0, 0, 0)
+		Ipv4Addr::from_bits(0)
 	}
 
 	fn new_all_true() -> Self {
-		Ipv4Addr::new(!0, !0, !0, !0)
+		Ipv4Addr::from_bits(!0)
 	}
 
 	fn contains(&self, prefix: usize, other: &Self) -> bool {
-		BigEndianBitString::bits_prefix_of(&self.octets()[..], prefix, &other.octets()[..])
+		self.to_bits().bits_prefix_of(prefix, &other.to_bits())
 	}
 }
 
@@ -82,9 +92,19 @@ where
 	result
 }
 
+fn with_ipv6_mut_u128<F, T>(addr: &mut Ipv6Addr, f: F) -> T
+where
+	F: FnOnce(&mut u128) -> T,
+{
+	let mut o = addr.to_bits();
+	let result = f(&mut o);
+	*addr = Ipv6Addr::from_bits(o);
+	result
+}
+
 impl FixedBitString for Ipv6Addr {
 	fn inc(&mut self, prefix: usize) -> bool {
-		with_ipv6_mut_slice(self, |slice| BigEndianBitString::bits_inc(slice, prefix))
+		with_ipv6_mut_slice(self, |slice| slice.bits_inc(prefix))
 	}
 
 	fn len() -> usize {
@@ -92,46 +112,46 @@ impl FixedBitString for Ipv6Addr {
 	}
 
 	fn get(&self, ndx: usize) -> bool {
-		BigEndianBitString::bit_get(&self.octets()[..], ndx)
+		self.octets().as_slice().bit_get(ndx)
 	}
 
 	fn set(&mut self, ndx: usize, bit: bool) {
-		with_ipv6_mut_slice(self, |slice| BigEndianBitString::bit_set(slice, ndx, bit))
+		with_ipv6_mut_slice(self, |slice| slice.bit_set(ndx, bit))
 	}
 
 	fn flip(&mut self, ndx: usize) {
-		with_ipv6_mut_slice(self, |slice| BigEndianBitString::bit_flip(slice, ndx))
+		with_ipv6_mut_slice(self, |slice| slice.bit_flip(ndx))
 	}
 
 	fn shared_prefix_len(&self, other: &Self, max_len: usize) -> usize {
-		BigEndianBitString::shared_prefix_len(&self.octets()[..], &other.octets()[..], max_len)
+		self.to_bits().shared_prefix_len(&other.to_bits(), max_len)
 	}
 
 	fn set_false_from(&mut self, ndx: usize) {
-		with_ipv6_mut_slice(self, |slice| BigEndianBitString::set_false_from(slice, ndx))
+		with_ipv6_mut_u128(self, |num| num.set_false_from(ndx))
 	}
 
 	fn is_false_from(&self, ndx: usize) -> bool {
-		BigEndianBitString::is_false_from(&self.octets()[..], ndx)
+		self.to_bits().is_false_from(ndx)
 	}
 
 	fn set_true_from(&mut self, ndx: usize) {
-		with_ipv6_mut_slice(self, |slice| BigEndianBitString::set_true_from(slice, ndx))
+		with_ipv6_mut_u128(self, |num| num.set_true_from(ndx))
 	}
 
 	fn is_true_from(&self, ndx: usize) -> bool {
-		BigEndianBitString::is_true_from(&self.octets()[..], ndx)
+		self.to_bits().is_true_from(ndx)
 	}
 
 	fn new_all_false() -> Self {
-		Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0)
+		Ipv6Addr::from_bits(0)
 	}
 
 	fn new_all_true() -> Self {
-		Ipv6Addr::new(!0, !0, !0, !0, !0, !0, !0, !0)
+		Ipv6Addr::from_bits(!0)
 	}
 
 	fn contains(&self, prefix: usize, other: &Self) -> bool {
-		BigEndianBitString::bits_prefix_of(&self.octets()[..], prefix, &other.octets()[..])
+		self.to_bits().bits_prefix_of(prefix, &other.to_bits())
 	}
 }
