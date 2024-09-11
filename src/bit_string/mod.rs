81690205035ce543687ae4567355a3753161afc8
Stefan Bühler <source@stbuehler.de>
2024-09-11 19:26:15 +0200

    reorganize bit_string / export

--- src/bit_string/mod.rs
+++
@@ -1,5 +0,0 @@
-//! contains everything related to the `BitString` trait.
-
-pub use self::traits::*;
-
-mod traits;
--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,14 @@
 #![doc(html_root_url = "https://docs.rs/bitstring/0.2.0-alpha1")]
 #![no_std]
 
-pub use self::bit_length_string::*;
-
-// re-export. sadly duplicates the doc pages...
 pub use self::{
+	bit_length_string::BitLengthString,
 	bit_string::BitString,
 	fixed_bit_string::traits::FixedBitString,
 };
 
 mod address;
 mod bit_length_string;
-pub mod bit_string;
+mod bit_string;
 pub mod fixed_bit_string;
 pub mod utils;
